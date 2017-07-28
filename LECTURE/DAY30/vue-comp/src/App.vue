fea7bf3dbb0e74ff7122b0224e405350c808fbf2
Jee Hoon <yamoo9@naver.com>
2017-07-28 14:20:59 +0900

    Vue.js Filter

--- LECTURE/DAY30/vue-comp/src/App.vue
+++ LECTURE/DAY30/vue-comp/src/App.vue
@@ -2,6 +2,8 @@
 
   #app
 
+    button.open-modal.button(@click="openModal") {{ 'Open Modal' | uppercase }}
+
     fds-message
       //- fds-message(heading="알림" class="is-warning")
       //-   p 오늘은 우리 수업의 마지막 날입니다. 행쇼~!
@@ -13,7 +15,14 @@
       //-   h3 {{ props.message }}
       p 오늘은 우리 수업의 마지막 날입니다. 행쇼~!
 
-    fds-modal(ref="my_modal" close_message="close lightbox")
+    fds-message
+      h3(slot="heading") 통화 변경
+      p 오늘 특가 이벤트! {{ 300 | currency('$') }}에 모십니다.
+
+    fds-modal(
+      ref="my_modal"
+      close_message="close lightbox"
+    )
       .image.is-16by9
         img(
           src="//lorempicsum.com/up/1280/720/2"
@@ -22,27 +31,32 @@
       //- .content
         //- h3 VueJS 컴포넌트
         //- p 하이 모달
-
-    button.button.is-info(
-      @click="openModal"
-    ) Open Modal
 </template>
 
 <script>
-import FdsMessage from './components/FDS/Message';
-import FdsModal from './components/FDS/Modal';
+  import FdsMessage from './components/FDS/Message';
+  import FdsModal from './components/FDS/Modal';
 
-export default {
-  name: 'app',
-  components: {
-    FdsMessage, FdsModal
-  },
-  methods: {
-    openModal(){
-      this.$refs.my_modal.visible = true;
+  export default {
+    name: 'app',
+    components: {
+      FdsMessage, FdsModal
+    },
+    filters: {
+      currency: (value, sign="원") => {
+        if ( sign !== '원' ) {
+          return sign + value;
+        } else {
+          return value + sign;
+        }
+      }
+    },
+    methods: {
+      openModal(){
+        this.$refs.my_modal.visible = true;
+      }
     }
   }
-}
 </script>
 
 <style lang="sass">
@@ -59,4 +73,7 @@ export default {
       left: auto
       right: auto
     // text-align: center
+
+  .open-modal
+    margin-bottom: 20px
 </style>
--- LECTURE/DAY30/vue-comp/src/main.js
+++ LECTURE/DAY30/vue-comp/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import App from './App';
 
+// 글로벌 Vue 필터 정의
+Vue.filter('uppercase', v => v.toString().toUpperCase());
+Vue.filter('lowercase', v => v.toString().toLowerCase());
+
 new Vue({
   el: '#app',
   render: h => h(App)
